1684b03024ca149d8359ad322620b046d5cb0f66
896369042@qq.com <sdsxstbatm1991>
2017-08-29 18:06:27 +0800

    update

---
+++ src/config/ports.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangjincheng
+ * Date: 16-7-14
+ * Time: 下午1:58
+ */
+
+use Server\CoreBase\PortManager;
+
+$config['ports'][] = [
+    'socket_type' => PortManager::SOCK_WS,
+    'socket_name' => '0.0.0.0',
+    'socket_port' => 8081,
+    'route_tool' => 'MyRoute',
+    'pack_tool' => 'NonJsonPack',
+    'opcode' => PortManager::WEBSOCKET_OPCODE_TEXT
+];
+
+return $config;
